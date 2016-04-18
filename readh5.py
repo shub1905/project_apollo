59f5659c376dea3cf7459c93db89f4130a92ecaf
shub1905 <shub1905@gmail.com>
2016-04-17 20:47:23 -0400

    File Data Reading Complete
    Sample Song names from dataset attached

--- readh5.py
+++ readh5.py
@@ -1,1 +1,28 @@
 import h5py
+import os
+
+
+def parse_data_files(folder='MillionSongSubset/data/'):
+    pwd = os.getcwd()
+    if not os.path.exists(pwd + '/' + folder):
+        print pwd + '/' + folder, 'Does not exists'
+        return
+
+    MediaPath = pwd + '/' + folder
+    file_path_gen = os.walk(MediaPath)
+    for root, dirs, file_names in file_path_gen:
+        for file_ in file_names:
+            yield root+'/'+file_
+
+
+def hdf_song_object():
+	gen = parse_data_files()
+	names = open('SongNames.txt','a')
+	for file_name in gen:
+		f = h5py.File(file_name, 'r')
+		f_meta = f['metadata']['songs']
+		f_title_indx = f_meta.dtype.names.index('title')
+		names.write(f_meta[0][f_title_indx]+'\n')
+	names.close()
+
+hdf_song_object()
